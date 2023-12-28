20f4439c0ec85697ab7bd4faa4c96e8c113c76be
Buwanding <buwandingaladdin@gmail.com>
2023-12-28 20:16:49 +0800

    History

---
+++ database/migrations/2023_12_28_121023_create_history_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('history', function (Blueprint $table) {
+            $table->increments('h_id');
+            $table->text('transaction');
+            $table->integer('d_id');
+            $table->string('name', 255);
+            $table->date('date');
+            $table->timestamps(); // Optional if you want 'created_at' and 'updated_at' columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('history');
+    }
+}
