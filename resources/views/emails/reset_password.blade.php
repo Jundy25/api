c47b7710b0008a33e91caac66d064376fde11c2b
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2024-01-03 04:34:50 +0800

    account

---
+++ resources/views/emails/reset_password.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/emails/reset_password.blade.php -->
+
+<p>Hello,</p>
+
+<p>You are receiving this email because we received a password reset request for your account.</p>
+
+<p>Your password reset token is: {{ $token }}</p>
+
+<p>If you did not request a password reset, no further action is required.</p>
+
+<p>Thank you.</p>
